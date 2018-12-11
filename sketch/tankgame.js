b0b5510d1bdb3d45ae09034d829aaeef2ac7b385
John Doe <sergioalemanb@gmail.com>
2018-12-11 16:40:59 -0600

    title

--- sketch/tankgame.js
+++ sketch/tankgame.js
@@ -14,7 +14,7 @@ var objetivo;
 var contEnemigos=200;
 var nuevoE;
 
-alert("BIENVENIDO A TESTE MARAVILLOSO JUEGO \n\n INDICACIONES  \n\n -> select level \n -> Se mueve con las flechas \n -> Dispara con espacio");
+alert("BIENVENIDO A TESTE MARAVILLOSO JUEGASO \n\n INDICACIONES  \n\n -> select level \n -> Se mueve con las flechas \n -> Dispara con espacio");
 
 function setup(nm) {
     //frameRate(2);
